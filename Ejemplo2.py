9126577fc694e71cc5e09d6a615e1e0f888d4a0c
AithusaTm <117110936+AithusaTm@users.noreply.github.com>
2023-05-09 13:03:04 -0600

    Update Ejemplo2.py

--- Ejemplo2.py
+++ Ejemplo2.py
@@ -3,12 +3,6 @@ print('hola Eli')
 print('saludos')
 print('Piñata')
 print('Hola Abraham')
-<<<<<<< HEAD
-<<<<<<< HEAD
 print('Elizabeth')
-=======
 print('Abraham')
->>>>>>> 2bb518398c3afebff0bff456d171153d3bc20719
-=======
 print('Alfredo')
->>>>>>> 945161d380aea0e97bf940ee5e8dad6a77efcc81
